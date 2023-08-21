98f6eee4b2afc3bb4e2fb040845f30eb8facbaac
CodeSandbox <support@codesandbox.io>
2023-08-21 13:35:59 +0000

    Initial commit

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,18 +1,21 @@
-<script>
-
+<script lang="ts">
+    import { menuItems } from "../config"
 </script>
 
-<nav class="fixed top-0 left-0 right-0 h-max bg-black/80 text-white border-b border-zinc-200">
-    <div class="p-8 md:p-10 lg:p-12 flex items-center gap-12 font-semibold">
+<nav class="fixed top-0 left-0 right-0 text-white border-b border-zinc-800">
+    <div class="px-8 py-6 md:p-10 lg:p-12 flex items-center gap-12 font-semibold">
         <a class="flex items-center gap-0" href="/">
             <span class="p-1.5 text-black/80 bg-white border border-transparent rounded-l">Mzizi</span>
             <span class="p-1.5 border border-current rounded-r">Agency</span>
         </a>
         <ul class="hidden md:flex md:items-center md:gap-8 mx-auto">
-            <li><a class="hover:text-emerald-500" href="/">Company</a></li>  
-            <li><a class="hover:text-emerald-500" href="/">Products</a></li>  
-            <li><a class="hover:text-emerald-500" href="/">Solutions</a></li>  
-            <li><a class="hover:text-emerald-500" href="/">Pricing</a></li>      
+            {#each menuItems as menuItem}
+                <li>
+                    <a class="hover:text-emerald-500" href={menuItem.href}>
+                        {menuItem.title}
+                    </a>
+                </li>
+            {/each} 
         </ul>
         <div class="w-max ml-auto flex items-center gap-12">
             <button 
@@ -21,6 +24,9 @@
                 Talk to us
             </button>
         </div>
+    </div>
+    <div class="absolute bg-black/50">
+    
     </div>
 </nav>
 
---
+++ src/lib/config.ts
@@ -0,0 +1,74 @@
+export const menuItems = [
+    {
+        title: "Company",
+        href:"/company", 
+        subItems:[
+            {
+                title: "About", 
+                href:"/about"
+            },
+            {
+                title: "Our Team",
+                href:"/team"
+            },
+            {
+                title: "Testimonials",
+                href:"/testimonials"
+            },
+        ]
+    },
+    {
+        title: "Products",
+        href:"/products", 
+        subItems:[
+            {
+                title: "Storefront", 
+                href:"/storefront"
+            },
+            {
+                title: "Wholelife",
+                href:"/wholelife"
+            },
+        ]
+    },
+    {
+        title: "Services",
+        href:"/services", 
+        subItems:[
+            {
+                title: "Custom Website", 
+                href:"/custom-website"
+            },
+            {
+                title: "Application Design",
+                href:"/app-desing"
+            },
+            {
+                title: "Custom App Development",
+                href:"/custom-app-development"
+            },
+            {
+                title: "IT Outsourcing",
+                href:"/it-outsourcing"
+            },
+        ]
+    },
+    {
+        title: "Pricing",
+        href:"/pricing", 
+        subItems:[
+            {
+                title: "Pricing Plans", 
+                href:"/pricing-plans"
+            },
+            {
+                title: "Pricing Calculator", 
+                href:"/pricing-calculator"
+            },
+            {
+                title: "Support Plans",
+                href:"/support-plans"
+            },
+        ]
+    }
+];
