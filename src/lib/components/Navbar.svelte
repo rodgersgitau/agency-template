cf701955a19dd65c8be46b24f1538da290c785ea
CodeSandbox <support@codesandbox.io>
2023-08-21 08:36:23 +0000

    Sandbox snapshot commit

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<nav class="fixed top-0 left-0 right-0 h-max bg-black/80 text-white border-b border-zinc-200">
+    <div class="p-8 md:p-10 lg:p-12 flex items-center gap-12 font-semibold">
+        <a class="flex items-center gap-0" href="/">
+            <span class="p-1.5 text-black/80 bg-white border border-transparent rounded-l">Mzizi</span>
+            <span class="p-1.5 border border-current rounded-r">Agency</span>
+        </a>
+        <ul class="hidden md:flex md:items-center md:gap-8 mx-auto">
+            <li><a class="hover:text-emerald-500" href="/">Company</a></li>  
+            <li><a class="hover:text-emerald-500" href="/">Products</a></li>  
+            <li><a class="hover:text-emerald-500" href="/">Solutions</a></li>  
+            <li><a class="hover:text-emerald-500" href="/">Pricing</a></li>      
+        </ul>
+        <div class="w-max ml-auto flex items-center gap-12">
+            <button 
+                class="px-3 py-2.5 border rounded-lg border-current hover:text-emerald-500"
+            >
+                Talk to us
+            </button>
+        </div>
+    </div>
+</nav>
+
